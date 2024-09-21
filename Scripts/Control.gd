5c377c320eac6425153a6600265374f358d036e7
Sancal0606 <carloz1.2.sanchez@gmail.com>
2024-09-21 22:43:10 +0200

    Ui improvements

--- Scripts/Control.gd
+++ Scripts/Control.gd
@@ -3,9 +3,9 @@ var simultaneous_scene = load("res://Scenes/game.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
-
+	Engine.time_scale = 1
+	$"../monkey_prueba/AnimationTree"["parameters/Movement/blend_amount"]
+	print("H")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
